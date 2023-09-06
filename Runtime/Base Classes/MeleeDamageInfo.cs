e8706765e2b220416e4ca0b24aa642ab919de357
SickBow <mikecilona@gmail.com>
2023-09-06 16:58:18 -0400

    Added public MeleeActor receiver

--- Runtime/Base Classes/MeleeDamageInfo.cs
+++ Runtime/Base Classes/MeleeDamageInfo.cs
@@ -8,6 +8,7 @@ public class MeleeHitInfo
     public int damage;
     public RaycastHit ray;
     public MeleeActor sender;
+    public MeleeActor receiver;
     public MeleeWeapon weapon;
     public AttackType attackType;
 }
