483696ae8ba4d4d80590386ab611f7917e566121
SickBow <mikecilona@gmail.com>
2024-02-07 11:09:37 -0500

    Added AnimatorStateInfo argument to InitializeAttack()

--- Runtime/Animator State Behaviors/ActivateAttack.cs
+++ Runtime/Animator State Behaviors/ActivateAttack.cs
@@ -23,7 +23,7 @@ public class ActivateAttack : StateMachineBehaviour
             damageStartTime = .3f; //default
             damageStopTime = .7f; //default
         }
-        _actor.InitializeAttack(attackType);
+        _actor.InitializeAttack(attackType, stateInfo);
         _attack = _actor.GetActiveAttack();
         _actor.AttackStart?.Invoke();
     }
