45d01c3e1f989f2a5a90c1b43c29fbcbb444b684
SickBow <mikecilona@gmail.com>
2024-02-22 14:43:39 -0500

    Added support for MeleeAttackDatas ScriptableObjects

--- Runtime/Animator State Behaviors/ActivateAttack.cs
+++ Runtime/Animator State Behaviors/ActivateAttack.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 namespace Sickbow.Melee{
 public class ActivateAttack : StateMachineBehaviour
 {
+    [SerializeField] MeleeAttackDatas attackDatas;
     [SerializeField] AttackType attackType;
     [SerializeField, Range(0,1)] float damageStartTime, damageStopTime;
+    [SerializeField] string meleeWeapon;
     private MeleeActor _actor;
     private MeleeAttack _attack;
     
@@ -16,6 +18,21 @@ public class ActivateAttack : StateMachineBehaviour
     
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        var clips = (animator.IsInTransition(layerIndex)) ? animator.GetNextAnimatorClipInfo(layerIndex) : animator.GetCurrentAnimatorClipInfo(layerIndex);
+        MeleeAttackData matchingAttackData = null;
+        if (clips.Length > 0) {
+            AnimationClip stateClip = clips[0].clip;
+            matchingAttackData = attackDatas.data.Find(data => data.GetAnimationClip() == stateClip);
+        }
+
+        if (matchingAttackData != null)
+        {
+            attackType = matchingAttackData.GetAttackType();
+            damageStartTime = matchingAttackData.GetDamageStartTime();
+            damageStopTime = matchingAttackData.GetDamageStopTime();
+            meleeWeapon = matchingAttackData.GetMeleeWeapon();
+        }
+        
         if (_actor == null)
             _actor = animator.transform.root.GetComponent<MeleeActor>();
 
@@ -23,7 +40,7 @@ public class ActivateAttack : StateMachineBehaviour
             damageStartTime = .3f; //default
             damageStopTime = .7f; //default
         }
-        _actor.InitializeAttack(attackType, stateInfo, damageStartTime, damageStopTime);
+        _actor.InitializeAttack(attackType, stateInfo, damageStartTime, damageStopTime, meleeWeapon);
         _attack = _actor.GetActiveAttack();
         _actor.AttackStart?.Invoke(_attack);
     }
