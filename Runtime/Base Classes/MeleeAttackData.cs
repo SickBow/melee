a3718c5205fd02d6007861ee7a00add206b1ce17
SickBow <mikecilona@gmail.com>
2024-03-07 16:16:07 -0500

    Added float field pushScale

--- Runtime/Base Classes/MeleeAttackData.cs
+++ Runtime/Base Classes/MeleeAttackData.cs
@@ -11,11 +11,13 @@ namespace Sickbow.Melee
         [SerializeField] string meleeWeapon;
         [SerializeField] AttackType attackType;
         [SerializeField] float damageStartTime, damageStopTime;
-
+        [SerializeField] float pushScale;
+        
         public AnimationClip GetAnimationClip() => clip;
         public string GetMeleeWeapon() => meleeWeapon;
         public AttackType GetAttackType() => attackType;
         public float GetDamageStartTime() => damageStartTime;
         public float GetDamageStopTime() => damageStopTime;
+        public float GetPushScale() => pushScale;
     }
 }
