a891e29af433d4d80aa707f385c47d964024c178
SickBow <mikecilona@gmail.com>
2024-07-05 19:52:05 -0400

    Added AnimationCurve fields

--- Runtime/Base Classes/MeleeAttackData.cs
+++ Runtime/Base Classes/MeleeAttackData.cs
@@ -13,7 +13,17 @@ namespace Sickbow.Melee
         [SerializeField] float damageStartTime, damageStopTime;
         [SerializeField] float pushScale;
         [SerializeField] int combo;
-        
+        [SerializeField] AnimationCurve orientSpeedCurve;
+        [SerializeField] AnimationCurve moveSpeedCurve;
+        [SerializeField] int topMoveSpeed;
+        [SerializeField] AnimationCurve forwardPushSpeedCurve;
+        [SerializeField] int topForwardPushSpeed;
+
+        public AnimationCurve GetOrientSpeedCurve() => orientSpeedCurve;
+        public AnimationCurve GetMoveSpeedCurve() => moveSpeedCurve;
+        public int GetTopMoveSpeed() => topMoveSpeed;
+        public AnimationCurve GetForwardPushSpeedCurve() => forwardPushSpeedCurve;
+        public int TopForwardPushSpeed() => topForwardPushSpeed;
         public AnimationClip GetAnimationClip() => clip;
         public string GetMeleeWeapon() => meleeWeapon;
         public AttackType GetAttackType() => attackType;
